6111f9b04e008567b5a24ede445c3d2a4f98391f
sunilsch <120412820+sunilsch@users.noreply.github.com>
2022-12-13 16:45:49 +0100

    simplify code

--- 2022/day13.py
+++ 2022/day13.py
@@ -1,6 +1,5 @@
 import ast
 from functools import cmp_to_key
-
 def compare(first, second):
     if type(first) == int and type(second) == int:
         return 0 if first == second else 1 if first < second else -1
@@ -16,15 +15,14 @@ def compare(first, second):
         return compare(first, [second])
 with open('day13.txt') as f:
     inp = [x for x in f.read().split("\n\n")]
-    val = [(ast.literal_eval(x),ast.literal_eval(y)) for x,y in (z.split("\n") for z in inp)]
-    val2 = []
-    for x in inp:
-        first, second = x.split()
-        val2.append(ast.literal_eval(first))
-        val2.append(ast.literal_eval(second))
-print("First star: ", sum(i+1 for i,(x,y) in enumerate(val) if compare(x,y) == 1))
-
+val = [(ast.literal_eval(x),ast.literal_eval(y)) for x,y in (z.split("\n") for z in inp)]
+val2 = []
+for x in inp:
+    first, second = x.split("\n")
+    val2.append(ast.literal_eval(first))
+    val2.append(ast.literal_eval(second))
 val2.append([2])
 val2.append([6])
-val2 = sorted(val2, key=cmp_to_key(compare), reverse=True)
+val2.sort(key=cmp_to_key(compare), reverse=True)
+print("First star: ", sum(i+1 for i,(x,y) in enumerate(val) if compare(x,y) == 1))
 print("Second star: ", (val2.index([2])+1) * (val2.index([6])+1))
