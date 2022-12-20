0360c088d6a4d30a37abaea8a1b2ee343ad46d42
sunilsch <120412820+sunilsch@users.noreply.github.com>
2022-12-20 16:14:56 +0100

    Solve day 20

---
+++ 2022/day20.py
@@ -0,0 +1,22 @@
+from collections import deque
+with open('day20.txt') as f:
+    numbersOrg = [(i,int(x.strip())) for i,x in enumerate(f.readlines())]
+numbersPart1 = deque(numbersOrg.copy())
+numbersPart2 = deque([(i,x*811589153) for i,x in numbersOrg])
+def solve(numbers,part2):
+    for (i,num) in numbersOrg:
+        if part2:
+            num *= 811589153
+        idx = numbers.index((i,num))
+        numbers.remove((i,num))
+        numbers.rotate(-num)
+        numbers.insert(idx,(i,num))
+    numbersList = [x for (_,x) in numbers]
+    zeroIdx = numbersList.index(0)
+    return sum(numbersList[(zeroIdx+1000*i) % len(numbersOrg)] for i in [1, 2, 3]), numbers
+print(solve(numbersPart1,False)[0])
+ans = 0
+for _ in range(10):
+    a, numbersPart2 = solve(numbersPart2,True)
+    ans += a
+print(a)
