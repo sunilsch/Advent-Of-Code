f049d6661875e44b0cb5382eae04e3f8f881a87d
Linus Schumann <120412820+sunilsch@users.noreply.github.com>
2024-12-11 22:33:11 +0100

    Refactor 2024 day 11

--- 2024/day11.py
+++ 2024/day11.py
@@ -1,23 +1,21 @@
 from collections import defaultdict
 inp = [int(x) for x in open('day11.txt').readline().split(" ")]
-save = defaultdict(lambda: -1)
+save = defaultdict(int)
 def helper(_num, _age):
     if _age == 0:
         return 1
-    if save[(_num, _age)] != -1:
+    if save[(_num, _age)]:
         return save[(_num, _age)]
     s = str(_num)
     if _num == 0:
-        st = helper(1, _age - 1)
-    elif len(s)%2 == 0:
-        a = s[:len(s)//2]
-        b = s[len(s)//2:]
-        st = helper(int(a), _age - 1) + helper(int(b), _age - 1)
+        save[(_num, _age)] = helper(1, _age - 1)
+    elif len(s) % 2 == 0:
+        a = s[:len(s) // 2]
+        b = s[len(s) // 2:]
+        save[(_num, _age)] = helper(int(a), _age - 1) + helper(int(b), _age - 1)
     else:
-        st = helper(_num * 2024, _age - 1)
-    save[(_num, _age)] = int(st)
-    return int(st)
-def solve(n):
-    return sum(helper(x,n) for x in inp)
+        save[(_num, _age)] = helper(_num * 2024, _age - 1)
+    return save[(_num, _age)]
+solve = lambda n: sum(helper(x, n) for x in inp)
 print("First star: ", solve(25))
 print("Second star: ", solve(75))
